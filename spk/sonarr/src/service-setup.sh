f449fd173fd23790fca5c6274da63a7b91edb4d4
mreid-tt <michael_reid@email.com>
2022-11-19 07:09:48 -0400

    Sonarr: avoid downgrade on package updates
    Attempted to incorporate the changes in https://github.com/SynoCommunity/spksrc/pull/5485 adapted to Sonarr

--- spk/sonarr/src/service-setup.sh
+++ spk/sonarr/src/service-setup.sh
@@ -5,6 +5,7 @@ MONO="${MONO_PATH}/mono"
 # Sonarr uses the home directory to store it's ".config"
 HOME_DIR="${SYNOPKG_PKGVAR}"
 CONFIG_DIR="${SYNOPKG_PKGVAR}/.config"
+SONARR_CONFIG_DIR="${CONFIG_DIR}/Sonarr"
 
 # Sonarr v2 -> v3 compatibility:
 if [ -f "${SYNOPKG_PKGDEST}/share/NzbDrone/NzbDrone.exe" ]; then
@@ -14,7 +15,7 @@ if [ -f "${SYNOPKG_PKGDEST}/share/NzbDrone/NzbDrone.exe" ]; then
 else
     # v3 installed
     SONARR="${SYNOPKG_PKGDEST}/share/Sonarr/Sonarr.exe"
-    PID_FILE="${CONFIG_DIR}/Sonarr/sonarr.pid"
+    PID_FILE="${SONARR_CONFIG_DIR}/sonarr.pid"
 fi
 
 # Allow correct Sonarr SPK version checking (v2 or v3)
@@ -34,6 +35,7 @@ if [ "$SYNOPKG_DSM_ARCH" == "88f6281" -o "$SYNOPKG_DSM_ARCH" == "88f6282" ]; the
     MONO="MONO_ENV_OPTIONS='-O=-aot,-float32' ${MONO}"
 fi
 
+# for DSM < 7 only:
 GROUP="sc-download"
 LEGACY_GROUP="sc-media"
 
@@ -43,24 +45,38 @@ SVC_WAIT_TIMEOUT=90
 
 service_postinst ()
 {
-    mkdir -p ${CONFIG_DIR}
-    set_unix_permissions "${CONFIG_DIR}"
+    # Move config.xml to .config
+    mkdir -p ${SONARR_CONFIG_DIR}
+    mv ${SYNOPKG_PKGDEST}/app/config.xml ${SONARR_CONFIG_DIR}/config.xml
+    
+    echo "Set update required"
+    # Make Sonarr do an update check on start to avoid possible Sonarr
+    # downgrade when synocommunity package is updated
+    touch ${SONARR_CONFIG_DIR}/update_required
+    
+    if [ ${SYNOPKG_DSM_VERSION_MAJOR} -lt 7 ]; then
+        set_unix_permissions "${CONFIG_DIR}"
+    fi
 }
 
 service_preupgrade ()
 {
-    # We have to account for legacy folder in the root
-    # It should go, after the upgrade, into /var/.config/
-    # The /var/ folder gets automatically copied by service-installer after this
-    if [ -d "${LEGACY_CONFIG_DIR}" ]; then
-        echo "Moving ${LEGACY_CONFIG_DIR} to ${CONFIG_DIR}"
-        mv ${LEGACY_CONFIG_DIR} ${CONFIG_DIR} >> ${INST_LOG} 2>&1
-    fi
-    if [ ! -d ${CONFIG_DIR} ]; then
-        # Create, in case it's missing for some reason
-        mkdir -p ${CONFIG_DIR} >> ${INST_LOG} 2>&1
+    if [ ${SYNOPKG_DSM_VERSION_MAJOR} -ge 7 ]; then
+        # ensure config is in @appdata folder
+        if [ -d "${LEGACY_CONFIG_DIR}" ]; then
+            if [ "$(realpath ${LEGACY_CONFIG_DIR})" != "$(realpath ${CONFIG_DIR})" ]; then
+                echo "Move ${LEGACY_CONFIG_DIR} to ${CONFIG_DIR}"
+                mv ${LEGACY_CONFIG_DIR} ${CONFIG_DIR} 2>&1
+            fi
+        fi
     fi
 
+    ## never update Sonarr distribution, use internal updater only
+    [ -d ${SYNOPKG_TEMP_UPGRADE_FOLDER}/backup ] && rm -rf ${SYNOPKG_TEMP_UPGRADE_FOLDER}/backup
+    echo "Backup existing distribution to ${SYNOPKG_TEMP_UPGRADE_FOLDER}/backup"
+    mkdir -p ${SYNOPKG_TEMP_UPGRADE_FOLDER}/backup 2>&1
+    rsync -aX ${SYNOPKG_PKGDEST}/share ${SYNOPKG_TEMP_UPGRADE_FOLDER}/backup/ 2>&1
+    
     # Is Installed Sonarr Binary Ver. >= SPK Sonarr Binary Ver.?
     CUR_VER=$(${MONO_PATH}/monodis --assembly ${SONARR} | grep "Version:" | awk '{print $2}')
     echo "Installed Sonarr Binary: ${CUR_VER}"
@@ -88,7 +104,13 @@ service_postupgrade ()
         set_unix_permissions "${SYNOPKG_PKGDEST}/share"
     fi
 
-    set_unix_permissions "${CONFIG_DIR}"
+    ## restore Sonarr distribution
+    if [ -d ${SYNOPKG_TEMP_UPGRADE_FOLDER}/backup/share ]; then
+        echo "Restore previous distribution from ${SYNOPKG_TEMP_UPGRADE_FOLDER}/backup"
+        rm -rf ${SYNOPKG_PKGDEST}/share/Lidarr/bin 2>&1
+        # prevent overwrite of updated package_info
+        rsync -aX --exclude=package_info ${SYNOPKG_TEMP_UPGRADE_FOLDER}/backup/share/ ${SYNOPKG_PKGDEST}/share 2>&1
+    fi
 
     # If backup was created before new-style packages
     # new updates/backups will fail due to permissions (see #3185)
